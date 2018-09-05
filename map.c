17cf6d779e59e0fb9e942edaf1af48fd990129a6
Marcus Monterroso <mmonterroso@berkeley.edu>
2018-09-04 22:07:26 -0700

    We are basically done with this file

--- map.c
+++ map.c
@@ -9,8 +9,8 @@ int recur(int i) {
     /* A stack allocated variable within a recursive function */
     int j = i;
 
-    /* TODO 1: Fix this so it prints the address of j */
-    printf("recur call %d: stack@ %p\n", i, j);
+    /* TODO  1: Fix this so it prints the address of j */
+    printf("recur call %d: stack@ %d\n", i, j);
 
     if (i > 0) {
         return recur(i - 1);
@@ -27,19 +27,19 @@ int main(int argc, char *argv[]) {
     int i;
 
     /* Dynamically allocate some stuff */
-    char *buf1 = malloc(100);
+    char *buf1 = (char*)malloc(100);
     /* ... and some more stuff */
-    char *buf2 = malloc(100);
+    char *buf2 = (char*)malloc(100);
 
     printf("_main  @ %p\n", main);
     printf("recur @ %p\n", recur);
 
     /* TODO 2: Fix to get address of the stack variable */
-    printf("_main stack: %p\n", i);
+    printf("_main stack: %d\n", i);
 
     /* TODO 3: Fix to get address of a static variable */
                                                              
-    printf("static data: %p\n", stuff);
+    printf("static data: %d\n", stuff);
                                                              
     printf("Heap: malloc 1: %p\n", buf1);
 
--- wc.c
+++ wc.c
@@ -1,4 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
+   
+    int i = 0;
+
+    FILE *file;
+    char *code;
+    size_t n = 0;
+    int c;
+    char *filename;
+
+    int linecount = 0;
+    int wordcount = 0;
+    int bytecount = 0;
+
+
+    if (argc == 1){
+	//printf("We have yet to complete this yet");
+	//exit(1);
+	file = stdin;
+    }
+
+    if (argc == 2){
+
+	filename = argv[1];
+	file = fopen(filename, "r");
+    }
+
+	//lets read our file and get the file size
+	//long size;
+	//FILE *file;
+	/*
+	FILE *f;
+	f = fopen(filename, "rb");
+	if (f == NULL) {
+	    printf("You attempted to open a file, and it didn't happen");
+	    exit(1);
+	}
+	fseek(f, 0, SEEK_END);
+	bytecount = ftell(f);
+	fclose(f);
+	
+	*/
+	
+	//we have a flag to see whether we start counting a word
+	int word = 0;
+
+	if (file) {
+	    while ((c = getc(file)) != EOF) {
+		//putchar(c);
+		
+		bytecount++;
+
+		if (c == 10){
+		    linecount++;
+		}
+
+		if (c == 10 || c == 32 || c == 9) {
+		    //now we check if wordprog is 0 or 1
+			if (word == 1){
+			    word = 0;
+			}    
+		}
+		else {
+		    //this means we should start counting!
+		    //A non space was found, when it was before
+		    if (word == 0){
+			word = 1;
+			wordcount++;
+		    }
+		}
+	    }
+	    fclose(file);
+        }
+
+    if (argc > 2){
+	printf("There are too many arguments, you have %d ",argc);
+	exit(1);
+    
+    }
+    
+    printf("  %d  %d %d %s\n",linecount,wordcount,bytecount,filename);
+
     return 0;
 }
+
+
 
