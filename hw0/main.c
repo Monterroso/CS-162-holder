570a110d98aa49bfb6809a7de3d8d0dbac5503cd
Marcus Monterroso <mmonterroso@berkeley.edu>
2018-09-05 20:47:12 -0700

    Okay lets test this out instead

---
+++ hw0/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/resource.h>
+
+int main() {
+    struct rlimit lim;
+    printf("testing");
+    printf("stack size: %ld\n", 0L);
+    printf("process limit: %ld\n", 0L);
+    printf("max file descriptors: %ld\n", 0L);
+    return 0;
+}
